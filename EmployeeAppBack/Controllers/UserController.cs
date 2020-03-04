4889b166106ab7fbea2afe4aeb5fe1c15e677798
unknown <tzviseliger@gmail.com>
2020-03-04 00:02:58 -0500

    initializing backend for IYE

---
+++ EmployeeAppBack/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeAppBack.DAL;
+using EmployeeAppBack.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAppBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("CorsPolicy")]
+
+    public class UserController : ControllerBase
+    {
+        private DALUser daluser;
+        public UserController(DALUser daluser)
+        {
+            this.daluser = daluser;
+        }
+        // GET api/Accounts/Accounts
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            return daluser.GetUsers();
+        }
+
+        [HttpGet("getImage")]
+        public ActionResult GetImage()
+        {
+            string file = "weddingprofile.jpg";
+            string path = @"C:\Users\Raizel Seliger\source\repos\IYEproj\emAppBack\EmployeeAppBack\images";
+            string filepath = Path.Combine(path, file);
+
+            return File(filepath, "image/jpg", Path.GetFileName(filepath));
+        }
+
+        [HttpGet("{uname}/{password}")]
+        public ActionResult<User> GetUser(string uname, string password)
+        {
+            return daluser.GetUser(uname, password);
+        }
+    }
+}
---
+++ EmployeeAppBack/DAL/DALUser.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeAppBack.Models;
+
+
+namespace EmployeeAppBack.DAL
+{
+    public class DALUser
+    {
+        private readonly string connectionString;
+
+        public DALUser(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+        // TODO api/user/id
+        public User GetUser(string uname, string password)
+        {
+            User user = GetUsers().FirstOrDefault(x => x.UserName == uname);
+            if (user.PasswordString == password)
+            {
+                return user;
+            }
+            return new User
+            {
+                AccountID = 0
+            };
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> users = new List<User>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(@"SELECT * FROM Users", conn);
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        users.Add(
+                            new User
+                            {
+                                UserID = Convert.ToInt32(reader["UserID"]),
+                                AccountID = Convert.ToInt32(reader["AccountID"]),
+                                UserFirstName = Convert.ToString(reader["UserFirstName"]),
+                                UserName = Convert.ToString(reader["UserName"]),
+                                UserEmailAddress = Convert.ToString(reader["UserEmailAddress"]),
+                                UserPhoneNumber = Convert.ToString(reader["UserPhoneNumber"]),
+                                UserType = Convert.ToString(reader["UserType"]),
+                                UserLastName = Convert.ToString(reader["UserLastName"]),
+                                PasswordString = Convert.ToString(reader["PasswordString"]),
+                                Salt = Convert.ToString(reader["Salt"])
+                            }
+                        );
+                    }
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+
+            return users;
+        }
+    }
+}
